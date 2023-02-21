5d8cdbc14c196f8c274c6c54a2dd7553703a88a9
CrusdCat <flak8001@gmail.com>
2023-02-21 14:54:42 +0900

    basic 2

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -1,16 +1,15 @@
 import Head from 'next/head'
 import dynamic from 'next/dynamic'
 import { Box, Container, Flex } from '@chakra-ui/react'
-//import Footer from '../footer'
 
 const Main = ({ children, router }) => {
   return (
     <Box as="main" pb={8}>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="MinHyeok's portfolio web" />
-        <meta name="author" content="MinHyeok Jeon" />
-        <title>Dev_Min Hyeok</title>
+        <meta name="description" content="Stage Play" />
+        <meta name="author" content="CWU CSE" />
+        <title>Stage_Play</title>
       </Head>
 
       {/* <NavBar path={router.asPath} /> */}
