16328fb5d095c1a3e9ebd58c0407bd530ad40456
jihwan <jhx1223@gmail.com>
2023-03-17 22:33:18 +0900

    iwantdatepicker

--- pages/reservation.js
+++ pages/reservation.js
@@ -6,90 +6,91 @@ import {
   Flex,
   Image,
   Table,
+  Thead,
   Tbody,
   Tfoot,
   Tr,
   Th,
   Td,
-  VStack,
+  TableCaption,
+  TableContainer,
   Tabs,
   TabList,
   TabPanels,
   Tab,
-  TabPanel
+  TabPanel,
+
 } from '@chakra-ui/react'
 //import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
+//import ImageSlider from '../components/MainImageSlider'
 import React from 'react'
-import ImageSlider from '../components/MainImageSlider'
 import { auto } from '@popperjs/core'
 
 
 const Page = () => {
     return (
-        <Container w="100%" mr={auto} ml={auto}>
-            <Flex>
-                
-            </Flex>
+        <Container>
             <Heading py={10}> 제목: 럭키 채준혁.ver</Heading>
-            <Box w="100%">
-            <Image
+            <Container >
+            <Flex>
+                <Box w={100}>
+                    <Image
                         src='https://bit.ly/dan-abramov' alt='Dan Abramov'
                         rounded="1rem"
                         shadow="2xl"
                         w="100%"
                         display="block"
-            />
-            </Box>
-            <Flex h={{ base: "auto", md: "100vh" }} py={10}
-            direction={{ base: "column-reverse", md: "row" }}>
-                <VStack w="full" h="full" p={0} spacing={0} alignItems="flex-start">
-
-                    <Tabs>
-                        <TabList>
-                            <Tab>공연 정보</Tab>
-                            <Tab>캐스팅 정보</Tab>
-                            <Tab>관람 후기</Tab>
-                            <Tab>Q & A</Tab>
-                        </TabList>
-
-                        <TabPanels>
-                            <TabPanel>
-                            <p><Table variant='simple'>
-                                <Tbody>
-                                    <Tr>
-                                        <Td>공연 장소</Td>
-                                        <Td>청운대학교 홍성캠퍼스 (444호)</Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td>공연 기간</Td>
-                                        <Td>2023/03/06 ~ 2023/03/07</Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td>공연 시간</Td>
-                                        <Td>12:00 ~ 14:00</Td>
-                                    </Tr>
-                                </Tbody>
-                                <Tfoot>
-                                    <Tr>
-                                    <Th>가격</Th>
-                                    <Th>1000만원</Th>
-                                    </Tr>
-                                </Tfoot>
-                            </Table></p>
-                            </TabPanel>
-                            <TabPanel>
-                                <p>채준혁</p>
-                            </TabPanel>
-                            <TabPanel>
-                                <p>잘생겼다</p>
-                            </TabPanel>
-                            <TabPanel>
-                                <p>질문있어요</p>
-                            </TabPanel>
-                        </TabPanels>
-                    </Tabs>
-                </VStack>
+                    />
+                </Box>
+                <Box>
+                <TableContainer>
+                    <Table variant='simple'>
+                            <Tbody>
+                                <Tr>
+                                <Td>공연 장소</Td>
+                                <Td>홍성캠퍼스</Td>
+                                </Tr>
+                                <Tr>
+                                <Td>공연 기간</Td>
+                                <Td>2023/03/17~2023/03/20</Td>
+                                </Tr>
+                                <Tr>
+                                <Td>공연 시간</Td>
+                                <Td>170분</Td>
+                                </Tr>
+                            </Tbody>
+                            <Tfoot>
+                                <Tr>
+                                <Th>가격</Th>
+                                <Th>A석 5000원</Th>
+                                </Tr>
+                            </Tfoot>
+                    </Table>
+                </TableContainer>
+                </Box>
             </Flex>
+            </Container>
+            <Tabs py={20}>
+                <TabList>
+                    <Tab>공연 정보</Tab>                       <Tab>캐스팅 정보</Tab>
+                    <Tab>관람 후기</Tab>
+                    <Tab>Q & A</Tab>
+                </TabList>
+                <TabPanels>
+                    <TabPanel>
+                        <div>알빠노</div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div>채준혁</div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div>잘생겼다</div>
+                    </TabPanel>
+                    <TabPanel>
+                        <div>질문있어요</div>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
         </Container>    
     )
 }
