7244b4efedea4eaaa871033620367ae9a7dfccff
jihwan <jhx1223@gmail.com>
2023-03-17 23:08:48 +0900

    datepickercomingsoon

--- pages/reservation.js
+++ pages/reservation.js
@@ -29,20 +29,22 @@ import { auto } from '@popperjs/core'
 
 const Page = () => {
     return (
-        <Container >
+        <Container m={0}>
             <Heading py={10}> 제목: 럭키 채준혁.ver</Heading>
-            <Flex>
-                <Box w="100%">
+            <Container>
+            <Flex w={800}>
+                <Box>
                     <Image
                         src='https://bit.ly/dan-abramov' alt='Dan Abramov'
                         rounded="1rem"
                         shadow="2xl"
-                        w="100%"
+                        width={400}
+                        height={400}
                         display="block"
                     />
                 </Box>
-                <Box>
-                <TableContainer>
+                <Box ml={10}>
+                <TableContainer mt={20}>
                     <Table variant='simple'>
                             <Tbody>
                                 <Tr>
@@ -71,65 +73,8 @@ const Page = () => {
             </Container>
             <Tabs py={20}>
                 <TabList>
-                    <Tab>공연 정보</Tab>                       <Tab>캐스팅 정보</Tab>
-                    <Tab>관람 후기</Tab>
-                    <Tab>Q & A</Tab>
-                </TabList>
-                <TabPanels>
-                    <TabPanel>
-                        <div>알빠노</div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>채준혁</div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>잘생겼다</div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>질문있어요</div>
-                    </TabPanel>
-                </TabPanels>
-            </Tabs>
-                    />
-                </Box>
-                <Box>
-                <TableContainer>
-                    <Table variant='simple'>
-                        <TableCaption>Imperial to metric conversion factors</TableCaption>
-                            <Thead>
-                                <Tr>
-                                <Th>To convert</Th>
-                                <Th>into</Th>
-                                <Th isNumeric>multiply by</Th>
-                                </Tr>
-                            </Thead>
-                            <Tbody>
-                                <Tr>
-                                <Td>inches</Td>
-                                <Td>millimetres (mm)</Td>
-                                <Td isNumeric>25.4</Td>
-                                </Tr>
-                                <Tr>
-                                <Td>feet</Td>
-                                <Td>centimetres (cm)</Td>
-                                <Td isNumeric>30.48</Td>
-                                </Tr>
-                            </Tbody>
-                            <Tfoot>
-                            <Tr>
-                            <Th>To convert</Th>
-                            <Th>into</Th>
-                            <Th isNumeric>multiply by</Th>
-                            </Tr>
-                            </Tfoot>
-                            </Table>
-                        </TableContainer>
-                </Box>
-            </Flex>
-            </Container>
-            <Tabs py={20}>
-                <TabList>
-                    <Tab>공연 정보</Tab>                       <Tab>캐스팅 정보</Tab>
+                    <Tab>공연 정보</Tab>                       
+                    <Tab>캐스팅 정보</Tab>
                     <Tab>관람 후기</Tab>
                     <Tab>Q & A</Tab>
                 </TabList>
