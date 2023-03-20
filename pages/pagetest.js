51b0088e50de99408fdb21a0b5690c376e248c47
chae1412 <jeoncr8381@gmail.com>
2023-03-20 13:16:03 +0900

    test

---
+++ pages/pagetest.js
@@ -0,0 +1,12 @@
+import { Button, ButtonGroup, input} from '@chakra-ui/react'
+
+const pagetest = () => {
+    return (
+        <Button colorScheme='blue'> Button 
+            <Input placeholder='Basic usage' />
+        </Button>
+
+    )
+}
+
+export default pagetest;
