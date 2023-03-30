aad6dc8a1bc6499b9fe8d429df3cb6a76a17ad7a
CrusdCat <flak8001@gmail.com>
2023-03-30 16:54:03 +0900

    test1

--- injectioncode.js
+++ injectioncode.js
@@ -35,13 +35,14 @@ function opt_checkSearchedWord(obj) {
         // Combine special characters and reserved keywords into a single regular expression
         var expText = /[=%><]|(?:OR|SELECT|INSERT|DELETE|UPDATE|CREATE|DROP|EXEC|UNION|FETCH|DECLARE|TRUNCATE)/gi;
         if (expText.test(obj)) {
-            alert("특수문자나 예약어를 입력 할 수 없습니다.");
+            console.log("특수문자나 예약어를 입력 할 수 없습니다.");
             obj = obj.replace(expText, "");
             return false;
         }
     }
+    console.log("test pass")
     return true;
 }
 
-test = "OR 123"
+test = "123"
 opt_checkSearchedWord(test);
