2bc8434064f607c040ffde93f0f186b591342c6b
wwmmIIll <cavin.jsk@gmail.com>
2023-05-03 20:09:28 +0900

    사이트간 연결 및 관리자 페이지 추가

---
+++ pages/announcement.js
@@ -0,0 +1,17 @@
+import NextLink from "next/link";
+import {
+    Text,
+
+} from "@chakra-ui/react";
+import React from "react";
+
+const announcement = () => {
+    return(
+        <Text>
+            공지사항 게시판입니다.
+        </Text>
+
+    );
+}
+
+export default announcement;
