54fff6e5c0c329ec35604140e5d3bfd8a4afb5bf
CrusdCat <flak8001@gmail.com>
2023-05-14 13:01:31 +0900

    ap

--- components/kakaomap.js
+++ components/kakaomap.js
@@ -22,21 +22,21 @@ function KakaoMap() {
     mapScript.addEventListener('load', onLoadKakaoMap);
 
 
-    // 마커가 표시될 위치입니다 
+    
     var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
 
-    // 마커를 생성합니다
+    
     var marker = new kakao.maps.Marker({
         position: markerPosition
     });
 
-    // 마커가 지도 위에 표시되도록 설정합니다
+    
     marker.setMap(map);
 
     var iwContent = '<div style="padding:5px;">Hello World! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
-        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); 
 
-    // 인포윈도우를 생성합니다
+    
     var infowindow = new kakao.maps.InfoWindow({
         position : iwPosition, 
         content : iwContent 
