4d6ffea24dc1412fcb3e18c020618fb6ba941ae6
CrusdCat <flak8001@gmail.com>
2023-05-14 13:00:31 +0900

    KakaoMap sdk

---
+++ components/kakaomap.js
@@ -0,0 +1,56 @@
+import { useEffect, useState } from 'react';
+
+function KakaoMap() {
+  useEffect(() => {
+    const mapScript = document.createElement('script');
+
+    mapScript.async = true;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_API_KEY}&autoload=false`;
+
+    document.head.appendChild(mapScript);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById('map');
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), 
+          level: 3, 
+        };
+        new window.kakao.maps.Map(mapContainer, mapOption);
+      });
+    };
+    mapScript.addEventListener('load', onLoadKakaoMap);
+
+
+    // 마커가 표시될 위치입니다 
+    var markerPosition  = new kakao.maps.LatLng(33.450701, 126.570667); 
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({
+        position: markerPosition
+    });
+
+    // 마커가 지도 위에 표시되도록 설정합니다
+    marker.setMap(map);
+
+    var iwContent = '<div style="padding:5px;">Hello World! <br><a href="https://map.kakao.com/link/map/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">큰지도보기</a> <a href="https://map.kakao.com/link/to/Hello World!,33.450701,126.570667" style="color:blue" target="_blank">길찾기</a></div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+        iwPosition = new kakao.maps.LatLng(33.450701, 126.570667); //인포윈도우 표시 위치입니다
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        position : iwPosition, 
+        content : iwContent 
+    });
+
+
+    infowindow.open(map, marker); 
+  }, []);
+
+  return (
+    <div>
+      <div id="map" className="w-96 h-96"></div>
+    </div>
+  );
+}
+
+export default KakaoMap;
