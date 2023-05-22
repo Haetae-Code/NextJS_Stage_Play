9f0b4bd3abddb4f39d56d13c170dc73152c4e69a
KMinKyoung <vldzmehowl32@naver.com>
2023-05-22 23:33:56 +0900

    1차 사이즈 조정

--- pages/admin.js
+++ pages/admin.js
@@ -31,32 +31,39 @@ const admin = () => {
         slidesToScroll: 4,
         afterChange: (index) => handleSliderChange(index),
       };
+
+
+
+
+
     //뮤지컬학과 데이터
     const dataMusical = [
         {
             image: 'https://www.m-i.kr/news/photo/202101/784601_561474_542.jpg',
             title: '오페라의 유령',
-            description: '04월 18일(화) 온라인, 강남구'
+            data: '04월 18일(화) 온라인,강남구'
+        
+
           },
           {
             image: 'https://www.kgnews.co.kr/data/photos/20220728/art_16578463029062_e1b76e.jpg',
             title: '마틸다',
-            description: '04월 18일(화) 온라인, 강남구'
+            data: '04월 18일(화) 온라인,강남구'
           },
           {
             image: 'https://image.yes24.com/images/chyes24/froala/0/44431/26305.jpg0',
             title: '문스토리',
-            description: '04월 18일(화) 온라인, 강남구'
+            data: '04월 18일(화) 온라인,강남구'
           },
           {
             image: 'https://newsimg.sedaily.com/2018/10/22/1S60FSQK8D_1.jpg',
             title: '그날들',
-            description: '04월 18일(화) 온라인, 강남구'
+            data: '04월 18일(화) 온라인,강남구'
           },
           {
             image: 'https://img.newspim.com/news/2018/11/01/1811011558557240.jpg',
             title: '젠틀맨스가이드-사랑과 살인편',
-            description: '04월 18일(화) 온라인, 강남구'
+            data: '04월 18일(화) 온라인,강남구'
           }
      
         ];
@@ -100,47 +107,49 @@ const admin = () => {
                     
                     {/* 등록 공연 목록 */}
                     
-                    <Box ml="30px" w="60%" >
-                    
-                      <Box >
+                    <Box ml="30px" w="100%">
+                      <Box ml="30px">
                         <Box w="90%">
-                        <Slider {...settings}>
-                        {dataMusical.map((item, index) => (
-                          <Box key={index} px={10}>
-                            <Card maxW="xl" maxH="lg" borderWidth="0" borderRadius="sm" overflow="hidden">
-                              <Image mt="30px" mx="auto" src={item.image} alt={item.title} style={{ width: '150px', height: '200px'}}/>
-                              <CardHeader>
-                                <Text fontSize="sm" fontWeight="bold"  mt={2} style={{whiteSpace: 'nowrap', textoverflow: 'ellipsis', width: '80%'}}>
-                                 {item.title}
-                                </Text>
-                              </CardHeader>
-                              <CardBody h="60px">
-                                <Text fontSize="md">{item.description}</Text>
-                              </CardBody>
-                              <CardFooter>
-                              <Button colorScheme="blue">예매하기</Button>
-                              </CardFooter>
-                            </Card>
-                          </Box>
-                          ))}
-                        </Slider>
-                        </Box>    
-                      </Box>
-                      
+                          <Slider {...settings} style={{ height: '100%' }}>
+                            {dataMusical.map((item, index) => (
+                             <Box key={index} px={10} style={{ width: '150px', maxHeight: '80%', overflow: 'hidden' }}>
+                               <Card maxH="md" height="lg" borderRadius="sm" overflow="hidden" style={{ width: '100%' }}>
+                                  <CardHeader>
+                                    <Image src={item.image} alt={item.title} style={{ width: '100%', objectFit: 'cover' }} />
+                                 </CardHeader>
+                                  <CardBody   overflow="hidden">
+                                   <Text fontSize="sm" fontWeight="bold" mt={2} style={{ whiteSpace: 'normal', width: '100%' }}>
+                                      {item.title}
+                                   </Text>
+                                    <Text fontSize="sm" style={{ whiteSpace: 'normal', overflow: 'hidden', textOverflow: 'ellipsis', width: '100%' }}>{item.data}</Text>
+                                  </CardBody>
+                                 <CardFooter display="flex" justifyContent="space-between" style={{ whiteSpace: 'normal' }}>
+                                   <Button size="sm" colorScheme="blue">조회</Button>
+                                    <Button size="sm" colorScheme="blue">수정</Button>
+                                 </CardFooter>
+                                </Card>
+                             </Box>
+                           ))}
+                          </Slider>
+                       </Box>
+                     </Box>
                     </Box>
 
+
                     {/* 공연 추가 */}
-                    <Box mt="150px" w="40%" h="100%"  >
-                        <Text fontSize="50px" mt="50px" mb="30px" textAlign="center">공연 관리</Text>
+                   
+                      
+                        
+                  
+                </Box>
+                <Box ml="30px" w="100%" >
+                <Text fontSize="20px" mt="50px" mb="30px" textAlign="center">공연 관리</Text>
                         <Flex align="center" direction="column">
                         
                         <Button mb={3}>공연 추가</Button>
                         <Button mb={3}>공연 삭제</Button>
                       </Flex>
-                        
-                    </Box>
                 </Box>
-                
                
                 {/* 편집 공간 */}
                 <Box  flex="1" ml="50px" mt="80px" border="1px solid" borderColor="inherit" overflow="auto" >
