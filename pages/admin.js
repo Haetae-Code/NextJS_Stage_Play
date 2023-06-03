b0cc4268f5edf00a08a57e0d96a89f7575ecc52b
KMinKyoung <vldzmehowl32@naver.com>
2023-06-03 19:24:52 +0900

    수정버튼활성화

--- pages/admin.js
+++ pages/admin.js
@@ -10,18 +10,43 @@ import {
     CardHeader,
     Image,
     Link,
+    Tabs,
+    TabList,
+    Tab,
+    TabPanels,
+    TabPanel,
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    Stack,
+    Heading,
+    TableContainer,
+    Tfoot
 } from "@chakra-ui/react";
 import React, { useState, Fragment, useEffect } from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import KakaoMap from "../components/kakaomap";
 
 const admin = () => {
     const [sliderIndex, setSliderIndex] = useState(0);
     const [slidesToShow, setSlidesToShow] = useState(3);
+    const [showCode, setShowCode] = useState(false);
+
+    const handleEditButtonClick = () => {
+      setShowCode(!showCode);
+    };
+
+
+
     const handleSliderChange = (index) => {
         setSliderIndex(index);
     };
+  
 
     useEffect(() => {
         // 뷰포트 크기 변화 감지하여 slidesToShow 값 업데이트
@@ -79,6 +104,33 @@ const admin = () => {
         },
     ];
 
+    const actorData = [
+        {
+          image: "https://bit.ly/dan-abramov",
+          name: "채준혁",
+          department: "컴퓨터공학과",
+          intro: "컴공 체육부",
+        },
+        {
+          image: "https://bit.ly/dan-abramov",
+          name: "김준서",
+          department: "컴퓨터공학과",
+          intro: "컴공 홍보부",
+        },
+        {
+          image: "https://bit.ly/dan-abramov",
+          name: "전민혁",
+          department: "컴퓨터공학과",
+          intro: "스터디 에이스",
+        },
+        {
+          image: "https://bit.ly/dan-abramov",
+          name: "최인서",
+          department: "컴퓨터공학과",
+          intro: "최인서맨",
+        },
+      ];
+    
     return (
         <div style={{ display: "flex" }}>
             {/* 1번 박스(관리자 사이드바/관리자페이지) */}
@@ -205,6 +257,7 @@ const admin = () => {
                                                     <Button
                                                         size="sm"
                                                         colorScheme="blue"
+                                                        onClick={handleEditButtonClick}
                                                     >
                                                         수정
                                                     </Button>
@@ -236,6 +289,7 @@ const admin = () => {
 
                 {/* 편집 공간 */}
                 <Box
+                    
                     flex="1"
                     ml="50px"
                     mt="80px"
@@ -244,6 +298,179 @@ const admin = () => {
                     overflow="auto"
                 >
                     <Text>하단에 출력됩니다!</Text>
+                    <Box py={10} ml="30px">
+                    {showCode && (
+        <Box>
+        <Box>
+            <Stack py={5}>
+                <Flex>
+                    <Button colorScheme="blue" size="xs">
+                        {" "}
+                        현재상영중{" "}
+                    </Button>
+                    <Button colorScheme="blue" size="xs">
+                        {" "}
+                        예매중{" "}
+                    </Button>
+                    <Button colorScheme="red" size="xs">
+                        {" "}
+                        D-100{" "}
+                    </Button>
+                </Flex>
+                <Heading size={"xl"}>
+                    제목:이젠 제목이 길어도 괜찮아요
+                </Heading>
+            </Stack>
+
+            <Flex
+                justify={"center"}
+                alignItems={"center"}
+                flexDirection={["column", "row"]}
+            >
+                <Box mb={[10, 0]} mr={[0, 10]}>
+                    <Image
+                        src="https://bit.ly/dan-abramov"
+                        alt="No image"
+                        shadow="2xl"
+                        display="block"
+                    />
+                </Box>
+                <Stack>
+                    <Box>
+                        <TableContainer>
+                            <Table variant="simple" size={["sm", "md"]}>
+                                <Tbody>
+                                    <Tr>
+                                        <Td>공연 장소</Td>
+                                        <Td>홍성캠퍼스</Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>공연 기간</Td>
+                                        <Td>2023/03/17~2023/03/20</Td>
+                                    </Tr>
+                                    <Tr>
+                                        <Td>공연 시간</Td>
+                                        <Td>170분</Td>
+                                    </Tr>
+                                </Tbody>
+                                <Tfoot>
+                                    <Tr>
+                                        <Th>가격</Th>
+                                        <Th>A석-5000원 B석-3000원</Th>
+                                    </Tr>
+                                </Tfoot>
+                            </Table>
+                        </TableContainer>
+                    </Box>
+                    <Box>
+                        <Link href="./reservationdetail">
+                            <Button
+                                w={"100%"}
+                                size={["sm", "md"]}
+                                border="2px"
+                                colorScheme="blue"
+                            >
+                                {" "}
+                                예매하기
+                            </Button>
+                        </Link>
+                    </Box>
+                </Stack>
+            </Flex>
+        </Box>
+
+        <Box>
+            <Tabs align="center" pt={"20"}>
+                <TabList>
+                    <Tab>공연 정보</Tab>
+                    <Tab>캐스팅 정보</Tab>
+                </TabList>
+
+                <TabPanels>
+                    <TabPanel>
+                        <Table>
+                            <Thead>
+                                <Tr>
+                                    <Th fontSize={"x-large"}>공연 장소</Th>
+                                </Tr>
+                            </Thead>
+                        </Table>
+                        <Table>
+                            <Tbody>
+                                <Tr>
+                                    <Td w={20}>장소</Td>
+                                    <Td>홍주문화회관</Td>
+                                </Tr>
+                                <Tr>
+                                    <Td w={20}>주소</Td>
+                                    <Td>충남 홍성군 홍성읍 내포로 164</Td>
+                                </Tr>
+                            </Tbody>
+                        </Table>
+                        <Box py={10}>
+                           
+                            <KakaoMap></KakaoMap>
+                        </Box>
+
+                        <Table>
+                            <Thead>
+                                <Tr>
+                                    <Th fontSize={"x-large"}>
+                                        포스터 위치
+                                    </Th>
+                                </Tr>
+                            </Thead>
+                        </Table>
+                        <Box pt={10}>
+                            <Image
+                                src="https://bit.ly/dan-abramov"
+                                alt="Dan Abramov"
+                            />
+                        </Box>
+                    </TabPanel>
+
+                    <TabPanel py={10}>
+                        <Stack>
+                           
+                        {actorData.map((item, index) => (
+              <Box key={index} mb={4} display="flex" alignItems="center">
+                <Image
+                  src={item.image}
+                  alt={item.name}
+                  borderRadius="full"
+                  boxSize="100px"
+                  mr={4}
+                />
+                <Box>
+                  <TableContainer>
+                    <Table variant="simple">
+                      <Tfoot>
+                        <Tr>
+                          <Th>이름</Th>
+                          <Th>{item.name}</Th>
+                        </Tr>
+                        <Tr>
+                          <Th>학과</Th>
+                          <Th>{item.department}</Th>
+                        </Tr>
+                        <Tr>
+                          <Th>소개</Th>
+                          <Th>{item.intro}</Th>
+                        </Tr>
+                      </Tfoot>
+                    </Table>
+                  </TableContainer>
+                </Box>
+              </Box>
+            ))}
+                        </Stack>
+                    </TabPanel>
+                </TabPanels>
+            </Tabs>
+        </Box>
+    </Box>
+        )}
+                                </Box>
                 </Box>
             </Box>
         </div>
