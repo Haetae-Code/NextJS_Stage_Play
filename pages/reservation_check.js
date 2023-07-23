b55235657c4a121048017d1e60843fb15c8e18aa
jihwan <jhx1223@gmail.com>
2023-07-23 19:39:57 +0900

    r_c 추가

--- pages/reservation_check.js
+++ pages/reservation_check.js
@@ -1,119 +1,55 @@
-import { CardFooter, CardBody, Image, CardHeader, Card, Box, Button, Container, Flex, Input, Select, Text, Heading, TableContainer, Table, Thead, Tr, Th, Tbody, Td, Tfoot } from "@chakra-ui/react";
-import { max } from "date-fns";
-import React, { useState, Fragment, useEffect } from "react";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
-import Reservation from "./reservation"; 
+import { Progress, CardFooter, CardBody, Image, CardHeader, Card, Box, Button, Container, Flex, Input, Select, Text, Heading, TableContainer, Table, Thead, Tr, Th, Tbody, Td, Tfoot, Stack } from "@chakra-ui/react";
 
 
 const reservationcheck = () => {
-    const [sliderIndex, setSliderIndex] = useState(0);
-    const [slidesToShow, setSlidesToShow] = useState(3);
-
-    const [actorData, setActorData] = useState(null);
-    const [showPage, setShowPage] = useState(false);
-
-    const handleClick = () => {
-        setShowPage(true);
-    };
-
-    const handleSliderChange = (index) => {
-        setSliderIndex(index);
-    };
-
-
-    useEffect(() => {
-        // 뷰포트 크기 변화 감지하여 slidesToShow 값 업데이트
-        const handleResize = () => {
-            if (window.innerWidth < 600) {
-                setSlidesToShow(1);
-            } else if (window.innerWidth < 960) {
-                setSlidesToShow(2);
-            } else {
-                setSlidesToShow(3);
-            }
-        };
-        window.addEventListener("resize", handleResize);
-
-        return () => {
-            window.removeEventListener("resize", handleResize);
-        };
-    }, []);
-
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: slidesToShow,
-        slidesToScroll: slidesToShow,
-        afterChange: (index) => handleSliderChange(index),
-    };
-
-    //뮤지컬학과 데이터
-    const dataMusical = [
-        {
-            image: "https://www.m-i.kr/news/photo/202101/784601_561474_542.jpg",
-            title: "오페라의 유령",
-            data: "04월 18일(화) 온라인,강남구",
-        },
-        {
-            image: "https://www.kgnews.co.kr/data/photos/20220728/art_16578463029062_e1b76e.jpg",
-            title: "마틸다",
-            data: "04월 18일(화) 온라인,강남구",
-        },
-        {
-            image: "https://image.yes24.com/images/chyes24/froala/0/44431/26305.jpg0",
-            title: "문스토리",
-            data: "04월 18일(화) 온라인,강남구",
-        },
-        {
-            image: "https://newsimg.sedaily.com/2018/10/22/1S60FSQK8D_1.jpg",
-            title: "그날들",
-            data: "04월 18일(화) 온라인,강남구",
-        },
-        {
-            image: "https://img.newspim.com/news/2018/11/01/1811011558557240.jpg",
-            title: "젠틀맨스가이드-사랑과 살인편",
-            data: "04월 18일(화) 온라인,강남구",
-        },
-    ];
-
     return (
         <Box>
             <Heading >예약자 리스트</Heading>
             <Flex justify={"space-between"}>
 
                 {/*예약자 검색 박스*/}
-                <Box mr={1} w="55%" mt={5} fontSize="md" borderRadius={15} border="1px solid dodgerblue" p={4}>
+                <Box mr={1} w="45%" mt={5} fontSize="md" borderRadius={15} border="1px solid dodgerblue" p={4}>
 
                     <Box align="center" justify="center">
 
                         <Flex >
-                            <Text mt={1}>
+                            <Text w="25%">
                                 이름: &nbsp;
                             </Text>
-                            <Input name="name" type="text" ml={10} placeholder="김준서" />
+                            <Input mb={2} w="full" name="name" type="text" />
                         </Flex>
 
                         <Flex >
-                            <Text mt={1}>
+                            <Text w="25%">
                                 선택: &nbsp;
                             </Text>
 
-                            <Select name="student" ml={10}>
-                                <option>없음</option>
+                            <Select mb={2} w="full" name="student">
+                                <option>모두</option>
                                 <option>재학생</option>
                                 <option>외부인</option>
                             </Select>
                         </Flex>
 
+                        <Flex>
+                            <Text w="25%">
+                                학번: &nbsp;
+                            </Text>
+                            <Input mb={2} w="full" name="name" type="text" />
+                        </Flex>
 
                         <Flex>
-                            <Text w={85} mt={1}>
+                            <Text w="25%">
+                                학과: &nbsp;
+                            </Text>
+                            <Input mb={2} w="full" name="name" type="text" />
+                        </Flex>
+
+                        <Flex>
+                            <Text w="25%">
                                 전화번호: &nbsp;
                             </Text>
-                            <Input name="name" type="text" ml={3} placeholder="010-xxxx-xxxx" />
+                            <Input mb={5} w="full" name="name" type="text" />
                         </Flex>
 
                         <Button colorScheme="blue" /*onClick={() => search()}*/>검색</Button>
@@ -121,91 +57,58 @@ const reservationcheck = () => {
                 </Box>
 
                 {/*공연 예약 현황 박스*/}
-                <Box  ml={1} w="45%" mt={5} fontSize="md" borderRadius={15} border="1px solid dodgerblue" p={4}>
-                    
-                        {/* 등록 공연 목록 */}
-                            {" "}
-                            {/* 카드 슬라이더의 높이를 100%로 설정 */}
-                            <Slider
-                                {...settings}
-                                style={{ height: "100%" }}
-                            >
-                                {dataMusical.map((item, index) => (
-                                    <Box
-                                        key={index}
-                                        px={10}
-                                        style={{
-                                            width: "150px",
-                                            maxHeight: "80%",
-                                            overflow: "hidden",
-                                            height: "auto",
-                                        }}
-                                    >
-                                        <Card
-                                            maxH="md"
-                                            height="lg"
-                                            borderRadius="sm"
-                                            overflow="hidden"
-                                            style={{ width: "100%" }}
-                                        >
-                                            <CardHeader>
-                                                <Image
-                                                    src={item.image}
-                                                    alt={item.title}
-                                                    style={{
-                                                        width: "100%",
-                                                        objectFit: "cover",
-                                                    }}
-                                                />
-                                            </CardHeader>
-                                            <CardBody overflow="hidden">
-                                                <Text
-                                                    fontSize="sm"
-                                                    fontWeight="bold"
-                                                    mt={2}
-                                                    style={{
-                                                        whiteSpace:
-                                                            "normal",
-                                                        width: "100%",
-                                                    }}
-                                                >
-                                                    {item.title}
-                                                </Text>
-                                                <Text
-                                                    fontSize="sm"
-                                                    style={{
-                                                        whiteSpace:
-                                                            "normal",
-                                                        overflow: "hidden",
-                                                        textOverflow:
-                                                            "ellipsis",
-                                                        width: "100%",
-                                                    }}
-                                                >
-                                                    {item.data}
-                                                </Text>
-                                            </CardBody>
-                                            <CardFooter
-                                                display="flex"
-                                                justifyContent="space-between"
-                                                style={{
-                                                    whiteSpace: "normal",
-                                                }}
-                                            >
-                                                <Text fontSize="sm">15</Text>
-                                                <Text fontSize="sm">/</Text>
-                                                <Text fontSize="sm">30</Text>
-                                            </CardFooter>
-                                        </Card>
-                                    </Box>
-                                ))}
-                            </Slider>
-                        </Box>
+                <Box mr={1} w="55%" mt={5} fontSize="md" borderRadius={15} border="1px solid dodgerblue" p={4}>
+                    <Flex>
+                        <Image w="50%" h="100%" src='https://bit.ly/dan-abramov' alt='Dan Abramov' mr={5}></Image>
+                        <Stack justify={"space-between"}>
+                            <Box>
+                                {/* 예약현황 백분율 */}
+                                <Flex>
+                                    <Text>
+                                        예약 현황 &nbsp;
+                                    </Text>
+                                    <Text>
+                                        80
+                                    </Text>
+                                    <Text>
+                                        /
+                                    </Text>
+                                    <Text>
+                                        100
+                                    </Text>
+                                </Flex>
+                                <Progress value={80} />
+                            </Box>
+
+                            {/* 공연설명 */}
+                            <Text>
+                                공연 제목:
+                            </Text>
+                            <Text>
+                                공연 장소:
+                            </Text>
+                            <Text>
+                                공연 날짜:
+                            </Text>
+                            <Text>
+                                공연 시간:
+                            </Text>
+                        </Stack>
+                    </Flex>
+                </Box>
 
             </Flex>
 
             {/*예약자 리스트 박스*/}
-            <Box mt={20} fontSize="md" borderRadius={15} border="1px solid dodgerblue" p={4}>
+            <Box overflowX="auto"
+                h="200px"
+                whiteSpace="nowrap"
+                px="32px" 
+                mt={10} 
+                fontSize="md" 
+                borderRadius={10} 
+                border="1px solid dodgerblue" 
+                p={4}>
                 <TableContainer>
                     <Table size='sm'>
                         <Thead>
@@ -219,10 +122,75 @@ const reservationcheck = () => {
                             </Tr>
                         </Thead>
 
+                        {/*예약자 확인*/}
                         <Tbody>
                             <Tr>
                                 <Td>20200371</Td>
                                 <Td>김준서</Td>
+                                <Td>재학생</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>박지환</Td>
+                                <Td>재학생</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>김민경</Td>
+                                <Td>재학생</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>최인서</Td>
+                                <Td>재학생</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>윤태성</Td>
+                                <Td>재학생</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>채준혁</Td>
+                                <Td>외부인</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>이준혁</Td>
+                                <Td>외부인</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>전민혁</Td>
+                                <Td>외부인</Td>
+                                <Td>010-7704-5971</Td>
+                                <Td>2023-07-05</Td>
+                                <Td><Button size="xs" colorScheme="blue" mr={1}>수정</Button><Button size="xs" colorScheme="blue">삭제</Button></Td>
+                            </Tr>
+                            <Tr>
+                                <Td>20200371</Td>
+                                <Td>전채린</Td>
                                 <Td>외부인</Td>
                                 <Td>010-7704-5971</Td>
                                 <Td>2023-07-05</Td>
