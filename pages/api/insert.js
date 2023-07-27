b67b44a5f1fe09f1e2c1bafec3390b56ed689077
chae1412 <jeoncr8381@gmail.com>
2023-07-27 19:17:18 +0900

    insert 수정

--- pages/api/insert.js
+++ pages/api/insert.js
@@ -4,36 +4,54 @@ const db = require("./db");
 const insert = nextConnect();
 
 insert.use((req, res, next) => {
-    next();
+  next();
 });
 
 insert.post(async (req, res) => {
-    try {
-        const { name, phone_number, say_actor } = req.body;
+  try {
+    const { name, phone_number, say_actor, userType, department, id, identity, time_key } = req.body;
+    
+    const audience_key = (await db.query("SELECT COALESCE(MAX(audience_key), 0) + 1 AS audience_key FROM Stage_Play_DB.Audience"))[0].audience_key;
 
-        //마지막 adience_key 조회 쿼리
-        const selectQuery =
-            "SELECT MAX(adience_key) as lastAdienceKey FROM Stage_Play_DB.Audience";
+    // insert Student or Outsider
+    if (userType === 'student') {
+      const student_key = (await db.query("SELECT COALESCE(MAX(student_key), 0) + 1 AS student_key FROM Stage_Play_DB.Student"))[0].student_key;
 
-        const result = await db.query(selectQuery);
+      const insertAudience = "INSERT INTO Stage_Play_DB.Audience (audience_key, name, phone_number, say_actor) VALUES (?, ?, ?, ?)";
+      const insertAudienceValues = [audience_key, name, phone_number, say_actor];
 
-        const lastAdienceKey = result[0].lastAdienceKey || 0; // 값이 없을 경우 0으로 초기화
+      const insertStudent = "INSERT INTO Stage_Play_DB.Student (student_key, audience_key, id, department) VALUES (?, ?, ?, ?)";
+      const insertStudentValues = [student_key, student_key, id, department];
 
-        const adience_key = lastAdienceKey + 1; // 마지막 값에 1을 더해 새로운 adience_key 생성
+      await db.query(insertAudience, insertAudienceValues);
+      await db.query(insertStudent, insertStudentValues);
+    } else if (userType === 'external') {
+      const outsider_key = (await db.query("SELECT COALESCE(MAX(outsider_key), 0) + 1 AS outsider_key FROM Stage_Play_DB.Outsider"))[0].outsider_key;
 
-        // 데이터 삽입 쿼리
-        const insertQuery =
-            "INSERT INTO Stage_Play_DB.Audience (adience_key, name, phone_number, say_actor) VALUES (?, ?, ?, ?)";
-        const insertValues = [adience_key, name, phone_number, say_actor];
+      const insertAudience = "INSERT INTO Stage_Play_DB.Audience (audience_key, name, phone_number, say_actor) VALUES (?, ?, ?, ?)";
+      const insertAudienceValues = [audience_key, name, phone_number, say_actor];
 
-        // 쿼리 실행
-        const insertResult = await db.query(insertQuery, insertValues);
+      const insertOutsider = "INSERT INTO Stage_Play_DB.Outsider (outsider_key, audience_key, identity) VALUES (?, ?, ?)";
+      const insertOutsiderValues = [outsider_key, outsider_key, identity];
 
-        res.status(200).json({ message: "Data inserted successfully" });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
+      await db.query(insertAudience, insertAudienceValues);
+      await db.query(insertOutsider, insertOutsiderValues);
+    } else {
+
+      res.status(400).json({ message: "Unsupported userType" });
+      return;
     }
+
+    // Insert into Reservation table regardless of userType
+    const insertReservation = "INSERT INTO Stage_Play_DB.Reservation (audience_key, time_key) VALUES (?, ?)";
+    const insertReservationValues = [userType === 'student' ? student_key : outsider_key, time_key];
+    await db.query(insertReservation, insertReservationValues);
+
+    res.status(200).json({ message: "Data inserted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 });
 
 export default insert;
