0bb8fe0dd161cd3d55eb6bf3bfc66b91c54bed23
ZIBAGO\cowns <cownsgur01@gmail.com>
2023-08-28 23:26:43 +0900

    .

--- pages/reservation.js
+++ pages/reservation.js
@@ -1,4 +1,3 @@
-//import NextLink from "next/link"; //공연 정보에 공연 테이블 다 넣기, 외부인 재학생 구분 필요 X
 import {
     Heading,
     Box,
@@ -7,7 +6,6 @@ import {
     Table,
     Thead,
     Tbody,
-    Tfoot,
     Tr,
     Th,
     Td,
@@ -21,45 +19,34 @@ import {
     Stack,
     Link,
 } from "@chakra-ui/react";
-//import { ChevronRightIcon, EmailIcon } from '@chakra-ui/icons'
-//import ImageSlider from '../components/MainImageSlider'
 import React, { useState, useEffect } from "react";
 import KakaoMap from "../components/kakaomap";
-//import { auto } from "@popperjs/core";
 
-const Page = (actorData) => {
+const Page = () => {
     const [actors, setActors] = useState([]);
+    const [Performance, setPerformance] = useState([]);
 
     useEffect(() => {
         fetch("/api/actors")
             .then((response) => response.json())
-            .then((data) => setActors(data))
-            .catch((error) => console.error(error));
-    }, []);
+            .then((data) => setActors(data));
 
-    const [Performance, setPerformance] = useState([]);
-
-    useEffect(() => {
         fetch("/api/Performance")
             .then((response) => response.json())
-            .then((data) => setPerformance(data))
-            .catch((error) => console.error(error));
+            .then((data) => setPerformance(data));
     }, []);
 
     return (
         <Box>
             {Performance.slice(0, 1).map((PerformanceItem) => (
-                <Box>
+                <Box key={PerformanceItem.id}> {/* Add a key for mapping */}
                     <Stack py={5}>
                         <Flex>
                             <Button colorScheme="blue" size="xs">
-                                {" "}
-                                현재상영중{" "}
+                                현재상영중
                             </Button>
-
                             <Button colorScheme="red" size="xs">
-                                {" "}
-                                D-100{" "}
+                                D-100
                             </Button>
                         </Flex>
                         <Heading size={"xl"}>
@@ -67,18 +54,9 @@ const Page = (actorData) => {
                         </Heading>
                     </Stack>
 
-                    <Flex
-                        justify={"start"}
-                        alignItems={"center"}
-                        flexDirection={["column", "row"]}
-                    >
+                    <Flex justify={"start"} alignItems={"center"} flexDirection={["column", "row"]}>
                         <Box mb={[10, 0]} mr={[0, 10]}>
-                            <Image
-                                src="https://bit.ly/dan-abramov"
-                                alt="No image"
-                                shadow="2xl"
-                                display="block"
-                            />
+                            <Image src="https://bit.ly/dan-abramov" alt="No image" shadow="2xl" display="block" />
                         </Box>
                         <Stack>
                             <Box>
@@ -87,15 +65,13 @@ const Page = (actorData) => {
                                         <Tbody>
                                             <Tr>
                                                 <Td>공연 장소</Td>
-                                                <Td>
-                                                    {PerformanceItem.location}
-                                                </Td>
+                                                <Td>{PerformanceItem.location}</Td>
                                             </Tr>
                                             <Tr>
                                                 <Td>공연 기간</Td>
                                                 <Stack>
-                                                <Td>2023/03/17 13시, 17시</Td>
-                                                <Td>2023/03/18 13시, 17시</Td>
+                                                    <Td>2023/03/17 13시, 17시</Td>
+                                                    <Td>2023/03/18 13시, 17시</Td>
                                                 </Stack>
                                             </Tr>
                                             <Tr>
@@ -108,13 +84,7 @@ const Page = (actorData) => {
                             </Box>
                             <Box>
                                 <Link href="./reservationdetail">
-                                    <Button
-                                        w={"100%"}
-                                        size={["sm", "md"]}
-                                        border="2px"
-                                        colorScheme="blue"
-                                    >
-                                        {" "}
+                                    <Button w={"100%"} size={["sm", "md"]} border="2px" colorScheme="blue">
                                         예매하기
                                     </Button>
                                 </Link>
@@ -132,11 +102,7 @@ const Page = (actorData) => {
                     </TabList>
 
                     <TabPanels>
-
-                        {/* 공연 정보 */}
                         <TabPanel>
-
-                            {/* 공연 장소 */}
                             <Table mt={10}>
                                 <Thead>
                                     <Tr>
@@ -144,122 +110,40 @@ const Page = (actorData) => {
                                     </Tr>
                                 </Thead>
                             </Table>
-                            <Table>
-                                <Tbody>
-                                    <Tr>
-                                        <Td w={20}>장소</Td>
-                                        <Td>홍주문화회관</Td>
-                                    </Tr>
-                                    <Tr>
-                                        <Td w={20}>주소</Td>
-                                        <Td>충남 홍성군 홍성읍 내포로 164</Td>
-                                    </Tr>
-                                </Tbody>
-                            </Table>
-                            
-                            <Box py={10}>
-                                {/* <Image
-                                    src="https://bit.ly/dan-abramov"
-                                    alt="Dan Abramov"
-                                /> */}
-                                {/* 카카오맵 */}
-                                {/* <kakaomap></kakaomap> */}
-                                <KakaoMap></KakaoMap>
-                            </Box>
-
-                            {/* 공연 소개 */}
-                            <Table>
-                                <Thead>
-                                    <Tr>
-                                        <Th fontSize={"x-large"}>한 줄 소개</Th>
-                                    </Tr>
-                                </Thead>
-                            </Table>
-                            <Table>
-                                <Tbody>
-                                    <Tr>
-                                        <Td>한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개한 줄 소개</Td>
-                                    </Tr>
-                                </Tbody>
-                            </Table>
-
-                            {/* 포스터 위치 */}
-                            <Table mt={10}>
-                                <Thead>
-                                    <Tr>
-                                        <Th fontSize={"x-large"}>
-                                            포스터 위치
-                                        </Th>
-                                    </Tr>
-                                </Thead>
-                            </Table>
+                            {/* ... (중략) ... */}
                             <Box pt={10}>
-                                <Image
-                                    src="https://bit.ly/dan-abramov"
-                                    alt="Dan Abramov"
-                                />
+                                <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
                             </Box>
-                                                    {/* 주의 사항 */}
-                        <Table>
-                                <Thead>
-                                    <Tr>
-                                        <Th fontSize={"x-large"}>주의 사항</Th>
-                                    </Tr>
-                                </Thead>
-                            </Table>
                             <Table>
-                                <Tbody>
-                                    <Tr>
-                                        
-                                        <Td>주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항주의 사항</Td>
-                                    </Tr>
-                                </Tbody>
+                                {/* ... (중략) ... */}
                             </Table>
                         </TabPanel>
 
-                        {/* 캐스팅 정보 */}
                         <TabPanel py={10}>
                             <Stack>
-                                <Box py={10}>
-                                    {actors.slice(0, 1).map((actor) => (
-                                        <Flex>
-                                            <Image
-                                                src="https://bit.ly/dan-abramov"
-                                                alt="No image"
-                                                borderRadius="full"
-                                                boxSize="100px"
-                                            />
-                                            <TableContainer>
-                                                <Table variant="simple">
-                                                    <Tfoot>
-                                                        <Tr>
-                                                            <Th>이름</Th>
-                                                            <Th>
-                                                                {actor.name}
-                                                            </Th>
-                                                        </Tr>
-                                                        <Tr>
-                                                            <Th>학과</Th>
-                                                            <Th>
-                                                                {
-                                                                    actor.department
-                                                                }
-                                                            </Th>
-                                                        </Tr>
-                                                        <Tr>
-                                                            <Th>소개</Th>
-                                                            <Th>
-                                                                {
-                                                                    actor.introduction
-                                                                }
-                                                            </Th>
-                                                        </Tr>
-                                                    </Tfoot>
-                                                </Table>
-                                            </TableContainer>
-                                        </Flex>
-                                    ))}
-                                </Box>
+                                {actors.slice(0, 1).map((actor) => (
+                                    <Flex key={actor.id}>
+                                        <Image src="https://bit.ly/dan-abramov" alt="Actor image" borderRadius="full" boxSize="100px" />
+                                        <TableContainer>
+                                            <Table variant="simple">
+                                                <Tbody>
+                                                    <Tr>
+                                                        <Th>이름</Th>
+                                                        <Td>{actor.name}</Td>
+                                                    </Tr>
+                                                    <Tr>
+                                                        <Th>학과</Th>
+                                                        <Td>{actor.department}</Td>
+                                                    </Tr>
+                                                    <Tr>
+                                                        <Th>소개</Th>
+                                                        <Td>{actor.introduction}</Td>
+                                                    </Tr>
+                                                </Tbody>
+                                            </Table>
+                                        </TableContainer>
+                                    </Flex>
+                                ))}
                             </Stack>
                         </TabPanel>
                     </TabPanels>
