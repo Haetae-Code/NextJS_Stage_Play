c8ab6457f8121fb4bc70b07a6e272a04367da3c9
chae1412 <jeoncr8381@gmail.com>
2023-11-15 22:57:17 +0900

    insert 불필요한 문장 제거

--- pages/api/insert.js
+++ pages/api/insert.js
@@ -66,36 +66,20 @@ async function insertAudience(name, phone_number, say_actor, userType, departmen
 }
 
 insert.post(async (req, res) => {
-  let transaction;
   try {
     transaction = await db.beginTransaction();
 
     const { performance_key, name, phone_number, say_actor, userType, department, studentID, identity, selectedDate, selectedTime, time_key } = req.body;
 
-    console.log("time_key:", time_key);
-
     const audience_key = await insertAudience(name, phone_number, say_actor, userType, department, studentID, identity);
 
     const insertReservationQuery = "INSERT INTO Stage_Play_DB.Reservation (audience_key, time_key) VALUES (?, ?)";
     const insertReservationValues = [audience_key, time_key];
-    console.log("Inserting reservation data:", insertReservationValues);
     await db.query(insertReservationQuery, insertReservationValues);
 
-    await db.commit(transaction);
 
-    const audienceResult = await db.query(`
-        SELECT A.audience_key, S.student_key, O.outsider_key, A.name, S.id, S.department, A.phone_number
-        FROM Stage_Play_DB.Audience A 
-        JOIN Stage_Play_DB.Student S ON A.audience_key = S.audience_key
-        JOIN Stage_Play_DB.Outsider O ON A.audience_key = O.audience_key
-        WHERE A.audience_key = ?
-      `, [audience_key]);
-    console.log(audienceResult);
     res.status(200).json({ message: "Data inserted successfully" });
   } catch (error) {
-    if (transaction) {
-      await db.rollback(transaction);
-    }
     console.error(error);
     res.status(500).json({ message: "Internal Server Error" });
   }
